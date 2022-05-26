c0932f6f4f2ea9496bc4f2ab1395e6650dcfd584
Jan Kopejtko <jan.kopejtko@email.cz>
2022-05-26 20:55:39 +0200

    update gui life

--- spaceInvaders/Assets/script/lifeCounter.cs
+++ spaceInvaders/Assets/script/lifeCounter.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class lifeCounter : MonoBehaviour
 {
-    public static int numberOfLife = 3;
-    public static void chackDead() 
+    private static int numberOfLife = 3;
+    private GameObject lifeGUI;
+    private List<GameObject> lifeList = new List<GameObject>();
+    private void addLifeToList() 
+    {
+        foreach (Transform child in transform)
+        {
+            //lifeList.Add(transform);
+        }
+    }
+    void Start()
+    {
+        
+    }
+    public void deductLife() 
+    {
+        deleteGUIlife();
+        numberOfLife--;
+    }
+    private void deleteGUIlife() 
+    {
+    }
+    public static void chackDead()
     {
         if(numberOfLife <= 0) 
         {
